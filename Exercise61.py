193204325da2eb52413f20d294160eda175d6e33
oanh43cslt2 <150053173+oanh43cslt2@users.noreply.github.com>
2023-11-12 11:05:27 +0700

    Create Exercise61.py

---
+++ Exercise61.py
@@ -0,0 +1,13 @@
+S=0
+i=0
+while True:
+    n=int(input('Enter n:'))
+    if n==0 and i==0:
+        print('Error!')
+        break
+    if n==0:
+        break
+    S+=n
+    i+=1
+if i!=0:
+    print('the average of a collection of values entered :',S/i)
