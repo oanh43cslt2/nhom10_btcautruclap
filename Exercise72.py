c9e8b7edc10a94baeebd5ebcc7ab559eb27d4161
huy27cslt2 <149955123+huy27cslt2@users.noreply.github.com>
2023-11-17 23:28:40 +0700

    Update Exercise72.py

--- Exercise72.py
+++ Exercise72.py
@@ -3,6 +3,6 @@ reverse=""
 for i in string:
     reverse=i+reverse
 if reverse==string:
-    print(f'{string} is a palindrome string')
+    print(f'{string} is a Palindrome string')
 else:
-     print(f'{string} is not a palindrome string')
+     print(f'{string} is not a Palindrome string')
